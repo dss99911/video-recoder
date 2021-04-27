e44c687d79df93feeb657408e4bdcce9554a65ca
Hyun <hyun@balancehero.com>
2021-04-28 05:26:13 +0900

    add

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
+# install mmpeg https://maskaravivek.medium.com/how-to-install-ffmpeg-on-ec2-running-amazon-linux-451e4a8e2694
