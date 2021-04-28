6203e1702d74541c2e4d13f2516cb85ae428a692
Hyun <hyun@balancehero.com>
2021-04-29 04:57:47 +0900

    add

--- video_recorder.py
+++ video_recorder.py
@@ -1,14 +1,21 @@
 import argparse
+from datetime import datetime, timedelta
 import os
 import signal
 import subprocess
 
 import time
 
+
 def record_real_time_stream(video_url, output_path, duration):
     script = f"youtube-dl -o {output_path} {video_url}"
     process = subprocess.Popen(script, shell=True, preexec_fn=os.setsid)
-    time.sleep(int(duration))
+    finish_time = datetime.now() + timedelta(0, int(duration))
+    while datetime.now() < finish_time:
+        time.sleep(5)
+        if process.poll() is not None: #terminated
+            raise ValueError("Recording process terminated")
+
     os.killpg(os.getpgid(process.pid), signal.SIGTERM)
     subprocess.run(f"mv {output_path}.part {output_path}", stdout=subprocess.PIPE, shell=True)
 
