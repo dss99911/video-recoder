173e3fe0b24d608abf5aed6fa02fda76f18f6746
Hyun <hyun@balancehero.com>
2021-04-29 02:04:55 +0900

    add

--- scheduler.sh
+++ scheduler.sh
@@ -1,2 +1,3 @@
 RECORDER_PATH=/home/ec2-user/tbn-recorder
-python3 $RECORDER_PATH"/video_recorder.py" -v http://radio2.tbn.or.kr:1935/daejeon/myStream/playlist.m3u8 -o ${RECORDER_PATH}"/video-$(date +"%Y-%m-%d").mp4" -d 3600
+OUTPUT_PATH=/home/ec2-user/static
+python3 $RECORDER_PATH"/video_recorder.py" -v http://radio2.tbn.or.kr:1935/daejeon/myStream/playlist.m3u8 -o ${OUTPUT_PATH}"/video-$(date -d '+9 hour' +"%Y-%m-%d").mp4" -d 7200
