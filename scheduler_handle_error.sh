6092e5d080c7d7b843506c0ee5c17d10f4f47239
hyun.kim <dss99911@gmail.com>
2022-08-20 12:58:59 +0900

    update

--- scheduler_handle_error.sh
+++ scheduler_handle_error.sh
@@ -1,4 +1,4 @@
 source /home/ec2-user/.bash_profile
 
-sh $RECORDER_PATH/scheduler.sh > $RECORDER_PATH/log/stdout.log 2> $RECORDER_PATH/log/stderr.log \
-  || sh $RECORDER_PATH/send_to_slack.sh
+sh "$RECORDER_PATH/scheduler.sh" > "$RECORDER_PATH/log/stdout$(date -d '+9 hour' +"%Y-%m-%dT%H:%M:%S").log" 2> "$RECORDER_PATH/log/stderr$(date -d '+9 hour' +"%Y-%m-%dT%H:%M:%S").log" \
+  || sh "$RECORDER_PATH/send_to_slack.sh"
