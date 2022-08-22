4a517efc98766b642763eb0e0d85a03f652e35b4
hyun.kim <dss99911@gmail.com>
2022-08-22 17:40:56 +0900

    update

--- video_recorder.py
+++ video_recorder.py
@@ -9,15 +9,18 @@ import time
 
 def record_real_time_stream(video_url, output_path, duration):
     script = f"youtube-dl -o {output_path} {video_url}"
-    process = subprocess.Popen(script, shell=True, preexec_fn=os.setsid)
-    finish_time = datetime.now() + timedelta(0, int(duration))
-    while datetime.now() < finish_time:
-        time.sleep(5)
-        if process.poll() is not None: #terminated
-            raise ValueError("Recording process terminated")
 
-    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-    subprocess.run(f"mv {output_path}.part {output_path}", stdout=subprocess.PIPE, shell=True)
+    try:
+        process = subprocess.check_output(script, shell=True, preexec_fn=os.setsid)
+        finish_time = datetime.now() + timedelta(0, int(duration))
+        while datetime.now() < finish_time:
+            time.sleep(5)
+            if process.poll() is not None:  # terminated
+                raise ValueError("Recording process terminated")
+        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        subprocess.run(f"mv {output_path}.part {output_path}", stdout=subprocess.PIPE, shell=True)
+    except Exception as e:
+        raise e
 
 
 if __name__ == "__main__":
